ee7dc8684efc1d80297769746efb22647bb8c33a
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-01-10 17:29:52 -0800

    stash

--- test/WhyLang.Compiler.Tests/Text/TextWindowTests.cs
+++ test/WhyLang.Compiler.Tests/Text/TextWindowTests.cs
@@ -1,11 +1,11 @@
 using Xunit;
 
-namespace WhyLang.Compiler.Tests 
+namespace WhyLang.Compiler.Tests
 {
-    public class TextWindowTests 
+    public class TextWindowTests
     {
         [Fact]
-        public void WhenInitializedTheWindowIsEmpty() 
+        public void WhenInitializedTheWindowIsEmpty()
         {
             var window = new TextWindow("Test Document");
 
@@ -14,13 +14,43 @@ namespace WhyLang.Compiler.Tests
         }
 
         [Fact]
-        public void TakeAddsNextCharacterToWindow() {
+        public void TakeAddsNextCharacterToWindow()
+        {
             var window = new TextWindow("Test Document");
 
             Assert.True(window.Take());
 
             Assert.Equal(new TextSpan(0, 1), window.Span);
-            Assert.Equal(new [] {'T'}, window.Content.ToArray());
+            Assert.Equal(new[] { 'T' }, window.Content.ToArray());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Foo")]
+        public void TakeReturnsFalseWhenNoMoreCharactersToAdd(string str)
+        {
+            var window = new TextWindow(str);
+            for (var i = 0; i < str.Length; i += 1)
+            {
+                Assert.True(window.Take());
+            }
+            Assert.False(window.Take());
+        }
+
+        public void AdvanceMovesWindowUpToEnd() {
+            var window = new TextWindow("This is a test");
+            Assert.True(window.Take());
+            Assert.True(window.Take());
+            Assert.True(window.Take());
+            Assert.True(window.Take());
+            Assert.True(window.Take());
+
+            Assert.Equal("This ", window.GetString());
+
+            window.Advance();
+
+            Assert.Equal(new TextSpan(5, 0), window.Span);
+            Assert.Equal(0, window.Content.Length);
         }
     }
 }
